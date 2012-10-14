46c191ae50287129307c8f6439eb98bff9426c30
uglymunky <gabriel@uglymunky.com>
2012-10-14 08:09:18 -0700

    improve unit test

--- test/spec/Object.js
+++ test/spec/Object.js
@@ -13,6 +13,7 @@ describe('.Object', function () {
 			expect(NewConstructor._super).toBeDefined();
 		});
 
+
 		it('returns a new Constructor function who\'s prototype gets extended with the supplied arguments', function () {
 			var NewConstructor = kiva.Object.extend({prop1: 'uno', prop2: 'dos'});
 
@@ -21,12 +22,14 @@ describe('.Object', function () {
 		});
 	});
 
+
 	describe('static function: .create()', function () {
 		it('returns an object instance of the current object', function () {
 			var myObj = kiva.Object.create();
 
 			expect(Object.getPrototypeOf(myObj)).toEqual(kiva.Object.prototype);
 		});
+
 
 		it('returns an object instance of the current object and extends it with the supplied arguments', function () {
 			var dummyProperties = {prop1: 'uno', prop2: 'dos'}
@@ -37,6 +40,7 @@ describe('.Object', function () {
 			});
 		});
 
+
 		it('sets some default properties', function () {
 			var myObj = kiva.Object.create();
 
@@ -44,13 +48,31 @@ describe('.Object', function () {
 		})
 	});
 
+
 	describe('instance method: .fetch()', function () {
+
 		it ('Throws an exception when .kivaSrc or .zipSrc are not defined on the instance', function () {
-			var obj = kiva.Object.create();
+			var obj = kiva.Object.create()
+			, ajaxSpy = spyOn($, 'ajax');
 
 			expect(function () {
 				obj.fetch();
 			}).toThrow();
+		});
+
+
+		it ('Makes a $.ajax call and returns the jqXHR results', function () {
+			var obj = kiva.Object.create()
+			, ajaxSpy = spyOn($, 'ajax').andReturn('ajaxResults')
+			, ajaxResults;
+
+			obj.kivaSrc = kiva.kivaSrc + '/loans';
+			obj.zipSrc = kiva.zipSrc + '/loans';
+
+			ajaxResults = obj.fetch();
+
+			expect(ajaxSpy).toHaveBeenCalled();
+			expect(ajaxResults).toBe('ajaxResults')
 		});
 	});
 });
