81772f51d314555379887c2649ef034560b7eb18
uglymunky <gabriel@uglymunky.com>
2012-10-15 00:23:10 -0700

    grunt: lint, concat, minify @todo add dox to grunt build @todo should I switch to r.js, browerify, or maybe jam.js?

--- grunt.js
+++ grunt.js
@@ -1,70 +1,78 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    meta: {
-      version: '0.1.0',
-      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '* http://PROJECT_WEBSITE/\n' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-        'YOUR_NAME; Licensed MIT */'
-    },
-    lint: {
-      files: ['src/*.js', 'test/spec/*.js']
-    },
-//    qunit: {
-//      files: ['test/**/*.html']
-//    },
-//    concat: {
-//      dist: {
-//        src: ['<banner:meta.banner>', '<file_strip_banner:lib/FILE_NAME.js>'],
-//        dest: 'dist/FILE_NAME.js'
-//      }
-//    },
-//    min: {
-//      dist: {
-//        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-//        dest: 'dist/FILE_NAME.min.js'
-//      }
-//    },
-//    watch: {
-//      files: '<config:lint.files>',
-//      tasks: 'lint qunit'
-//    },
-    jshint: {
-      options: {
-      	"browser": true
-      	, "bitwise": true
-      	, "curly": true
-          , "eqeqeq": false
-          , "immed": true
-          , "latedef": true
-          , "newcap": true
-          , "node": true
-      	, "noarg": true
-          , "noempty": true
-          , "nonew": true
-      	, "smarttabs": true
-          , "strict": true
-          , "trailing": true
-      	, "undef": true
-      	, "laxbreak": true
-      	, "laxcomma": true
-	      , "proto": true
-      	, "validthis": true
-      	, "jquery": true
-	      , predef: ['kiva']
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
+	// Project configuration.
+	grunt.initConfig({
+		meta: {
+			version: '0.1.0'
+			, banner: '/*! kiva.js - v<%= meta.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'* http://kiva.org/\n' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+				'kiva.org; Licensed MIT */'
+		}
+		, lint: {
+			files: ['src/*.js', 'test/spec/*.js']
+		}
+		, concat: {
+			dist: {
+				src: ['<banner:meta.banner>', 'src/*.js']
+				, dest: 'kiva.js'
+			}
+		}
+		, min: {
+			dist: {
+				src: ['<banner:meta.banner>', '<config:concat.dist.dest>']
+				, dest: 'kiva.min.js'
+			}
+		}
+		//   watch: {
+		//      files: '<config:lint.files>',
+		//      tasks: 'lint qunit'
+		//    }
+		, dox: {
+			files: {
+				src: ['kiva.js']
+				, dest: 'docs'
+			}
+		}
+		, jshint: {
+			options: {
+				"browser": true
+				, "bitwise": true
+				, "curly": true
+				, "eqeqeq": false
+				, "immed": true
+				, "latedef": true
+				, "newcap": true
+				, "node": true
+				, "noarg": true
+				, "noempty": true
+				, "nonew": true
+				, "smarttabs": true
+				, "strict": true
+				, "trailing": true
+				, "undef": true
+				, "laxbreak": true
+				, "laxcomma": true
+				, "proto": true
+				, "validthis": true
+				, "jquery": true
+				, predef: ['kiva']
+			}
+			, globals: {
+				jQuery: true
+			}
+		}
+		, uglify: {}
+	});
 
-  // Default task.
-  grunt.registerTask('default', 'lint');
+
+	// Add dox https://github.com/punkave/grunt-dox
+	grunt.loadNpmTasks('grunt-dox');
+
+
+	// Default task.
+	grunt.registerTask('default', 'lint concat min dox');
 
 };
---
+++ kiva.js
@@ -0,0 +1,165 @@
+/*! kiva.js - v0.1.0 - 2012-10-14
+* http://kiva.org/
+* Copyright (c) 2012 kiva.org; Licensed MIT */
+
+
+(function (global) {
+	'use strict';
+
+	var  version = 1
+	, kivaApiVersion = 1
+	, zipApiVersion = 1
+	, kivaHost = 'api.kivaws.org'
+	, zipHost = 'zip.kiva.org'
+
+	, kivaPath = '/v'
+	, zipPath = '/v';
+
+
+	/**
+	 * @type {Object}
+	 */
+	global.kiva = {
+		ver: version
+		, kVer: kivaApiVersion
+		, zVer: zipApiVersion
+		, kivaSrc: 'http://' + kivaHost + kivaPath + kivaApiVersion
+		, zipSrc: 'http://' + zipHost + zipPath + zipApiVersion
+	};
+
+}(this));
+(function ($) {
+	'use strict';
+
+	kiva.Lenders = kiva.Object.extend({
+		name: 'Lenders'
+		, kivaSrc: kiva.kivaSrc + '/lenders'
+		, zipSrc: kiva.zipSrc + '/lenders'
+		, fetch: function (args) {
+			return kiva.RequestObject.prototype.fetch.call(this, args);
+		}
+	});
+}(jQuery));
+
+(function ($) {
+	'use strict';
+
+	kiva.Loans = kiva.RequestObject.extend({
+		name: 'Loans'
+		, kivaSrc: kiva.kivaSrc + '/loans'
+		, zipSrc: kiva.zipSrc + '/loans'
+		, fetch: function (args) {
+			return kiva.RequestObject.prototype.fetch.call(this, args);
+		}
+	});
+}(jQuery));
+
+(function ($) {
+	'use strict';
+
+	// @todo Probably need to revisit this code
+	// @hack to get around jshint (https://github.com/jshint/jshint/issues/525)
+	var Fn = Function;
+
+
+	/**
+	 *
+	 * @constructor
+	 */
+	kiva.Object = function () {};
+
+
+	/**
+	 * Creates a new Constructor function that "inherits" from "this".
+	 *
+	 * @param args
+	 * @return {*}
+	 */
+	kiva.Object.extend = function (args) {
+		var Child = args && args.name
+			? new Fn ('return function ' + args.name + ' () {}')()
+			: function () {};
+
+		Child.prototype = $.extend({}, this.prototype, args);
+		Child.prototype.__proto__ = this.prototype;
+		Child._super = this;
+		Child = $.extend(Child, this);
+
+		return Child;
+	};
+
+
+	/**
+	 * Creates a new object that inherits from "this"
+	 *
+	 * @param args
+	 */
+	kiva.Object.create = function (args) {
+		// @todo again, this if for jshint...
+		var This = this;
+		var newObj = new This();
+
+		return $.extend(newObj, args);
+	};
+
+}(jQuery));
+
+
+(function () {
+	'use strict';
+
+	kiva.RequestObject = kiva.Object.extend({
+
+		name: 'RequestObject'
+
+		, kivaSrc: kiva.kivaSrc
+
+		, zipSrc: kiva.zipSrc
+
+		, members: []
+
+
+		/**
+		 *
+		 * @param args
+		 * @returns jQuery.Deferred
+		 */
+		, fetch: function (args) {
+			var url = this.kivaSrc
+			, ids
+			, action
+			, self;
+
+			if (args) {
+				ids = args.ids;
+				action = args.action;
+
+				if ($.isArray(ids)) {
+					url = url + '/' + ids.join(',');
+				} else if (ids) {
+					throw 'Error: ids must be an array';
+				}
+
+				if (action) {
+					url = url + '/' + action;
+				}
+
+				url = url + '.json';
+			}
+
+			var $result = $.getJSON(url);
+
+			if (this.members) {
+				self = this;
+
+				$result.done(function (response) {
+					self.members = response[self.name.toLowerCase()];
+				});
+			}
+
+			return $result;
+		}
+	});
+
+}());
+
