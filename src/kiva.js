4f4c3d645c55419b33b026c5e257d670b5d96790
uglymunky <gabriel@uglymunky.com>
2012-10-16 11:33:01 -0700

    quick fix to allow for testing directly in the browser

--- src/kiva.js
+++ src/kiva.js
@@ -1,21 +1,24 @@
-var  version = 1
-, kivaApiVersion = 1
-, zipApiVersion = 1
-, kivaHost = 'api.kivaws.org'
-, zipHost = 'zip.kiva.org'
+(function ($, global) {
 
-, kivaPath = '/v'
-, zipPath = '/v';
+	var  version = 1
+	, kivaApiVersion = 1
+	, zipApiVersion = 1
+	, kivaHost = 'api.kivaws.org'
+	, zipHost = 'zip.kiva.org'
+
+	, kivaPath = '/v'
+	, zipPath = '/v';
 
 
-/**
- * @type {Object}
- */
-global.kiva = {
-	ver: version
-	, kVer: kivaApiVersion
-	, zVer: zipApiVersion
-	, kivaSrc: 'http://' + kivaHost + kivaPath + kivaApiVersion
-	, zipSrc: 'http://' + zipHost + zipPath + zipApiVersion
-};
+	/**
+	 * @type {Object}
+	 */
+	global.kiva = {
+		ver: version
+		, kVer: kivaApiVersion
+		, zVer: zipApiVersion
+		, kivaSrc: 'http://' + kivaHost + kivaPath + kivaApiVersion
+		, zipSrc: 'http://' + zipHost + zipPath + zipApiVersion
+	};
 
+}(jQuery, this));
