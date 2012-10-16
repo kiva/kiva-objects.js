58cdd3bb2a241fa68f0aef266ad7615a563fd92c
uglymunky <gabriel@uglymunky.com>
2012-10-16 13:44:44 -0700

    For now I have to choose, either testing in browser or via phantom, unfortunately can't do both Will have to revisit this later as there are a few ways I can think of addressing this, for now I just need to move on

--- src/kiva.js
+++ src/kiva.js
@@ -1,24 +1,20 @@
-(function ($, global) {
+var  version = 1
+, kivaApiVersion = 1
+, zipApiVersion = 1
+, kivaHost = 'api.kivaws.org'
+, zipHost = 'zip.kiva.org'
 
-	var  version = 1
-	, kivaApiVersion = 1
-	, zipApiVersion = 1
-	, kivaHost = 'api.kivaws.org'
-	, zipHost = 'zip.kiva.org'
-
-	, kivaPath = '/v'
-	, zipPath = '/v';
+, kivaPath = '/v'
+, zipPath = '/v';
 
 
-	/**
-	 * @type {Object}
-	 */
-	global.kiva = {
-		ver: version
-		, kVer: kivaApiVersion
-		, zVer: zipApiVersion
-		, kivaSrc: 'http://' + kivaHost + kivaPath + kivaApiVersion
-		, zipSrc: 'http://' + zipHost + zipPath + zipApiVersion
-	};
-
-}(jQuery, this));
+/**
+ * @type {Object}
+ */
+global.kiva = {
+	ver: version
+	, kVer: kivaApiVersion
+	, zVer: zipApiVersion
+	, kivaSrc: 'http://' + kivaHost + kivaPath + kivaApiVersion
+	, zipSrc: 'http://' + zipHost + zipPath + zipApiVersion
+};
